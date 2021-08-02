67341ba6c1f9e686ea8cb2974992989e496763a4
Shubham Jain <Shubham.jain05@outlook.com>
2021-08-02 23:10:58 +0530

    grph ql fix

--- src/GraphQL/resolver.ts
+++ src/GraphQL/resolver.ts
@@ -1,24 +1,12 @@
-import Product from "../models/noSql/product";
+import Post from "../models/noSql/post";
+import User from "../models/noSql/user";
 
 const resolve = {
-  createProduct: async function ({ ...productInput }, request: any) {
-    const existingProduct = await Product.findOne({
-      _title: productInput.title,
-    });
-    if (existingProduct) {
-      const error = new Error("Product exists already!");
-      throw error;
-    }
-
-    const product = new Product({
-      title: productInput.title,
-      price: productInput.price,
-      description: productInput.description,
-      imageUrl: productInput.imageUrl,
-      userId: productInput.userId,
-    });
-    const createdProduct: any = await product.save();
-    return { ...createdProduct._doc, _id: createdProduct._id.toString() };
+  hello() {
+    return {
+      text: "test graphql view",
+      views: 1234,
+    };
   },
 };
 
--- src/GraphQL/schema.ts
+++ src/GraphQL/schema.ts
@@ -1,35 +1,14 @@
 import { buildSchema } from "graphql";
 
 export default buildSchema(`
-    type Product {
-        _id: ID!
-        title: String!
-        price: String!
-        description: String!
-        imageUrl: String!
-        userId: String!
-        createdAt: String!
-        updatedAt: String!
+    type testData {
+        text: String!,
+        views: Int!
     }
-
-    input ProductInputData {
-        title: String!
-        price: String!
-        description: String!
-        imageUrl: String!
-        userId:String!
-    }
-  
     type RootQuery {
-        hello: String
-    } 
-    
-    type RootMutation {
-      createProduct(productInput: ProductInputData): Product!
+        hello: testData!
     }
-
     schema {
-      query: RootQuery
-      mutation: RootMutation
+        query: RootQuery
     }
 `);
